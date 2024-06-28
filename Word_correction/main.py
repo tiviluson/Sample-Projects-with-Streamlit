f9ebfae698cffc1c4e9b315931ebeb1ed78f8bda
hung <tiviluson@gmail.com>
2024-06-27 21:30:47 -0400

    fix: Update input correctly when clearing or choosing suggestions (no double Enter needed)

--- Word_correction/main.py
+++ Word_correction/main.py
@@ -1,14 +1,23 @@
 import streamlit as st
 
-@st.cache
+st.set_page_config(page_title='Word Correction', page_icon=':writing_hand:', menu_items={
+                   'About': "Made by [:rainbow[*tiviluson*] :sunglasses: :raccoon:](https://github.com/tiviluson) "})
+st.title('Word Correction using Levenshtein distance')
+st.logo('../assets/logo.png')
+st.caption(
+    'by [:rainbow[*tiviluson*] :sunglasses: :raccoon:](https://github.com/tiviluson) ')
+
+
+@st.cache_data
 def load_vocab(file_path) -> list:
     with open(file_path, 'r', encoding='utf-8') as f:
         vocab = f.read().splitlines()
         vocab.sort()
-    # print(vocab[:10])
+        print('Vocab loaded')
     return vocab
 
 
+# Unuseful when cached with st.cache_data
 def levenshtein_distance(input_word, output_word):
     if (input_word, output_word) in st.session_state.distance_cache:
         return st.session_state.distance_cache[(input_word, output_word)]
@@ -40,27 +49,18 @@ def get_levenstein_distances(input_word, vocab, k=5):
     return distances[:k]
 
 
-def choose_corrected_word(input_word_placeholder, output_text_placeholder, option_placeholders, buttons, suggestions):
-    # Checck which button is clicked
-    corrected_word = None
-    for i, (_, button) in enumerate(buttons):
-        if button:
-            corrected_word = suggestions[i][0]
-            break
-    if corrected_word:
-        print('Corrected word:', corrected_word)
-        clear_elements([output_text_placeholder, *option_placeholders])
-        st.session_state['last_word'] = corrected_word
-        input_word_placeholder.empty()
-        input_word_placeholder.text_input(
-            'Input word', value=corrected_word)
+def update_input(word, output_text_placeholder, option_placeholders):
+    st.session_state.input_word = word
+    clear_elements([output_text_placeholder, *option_placeholders])
 
 
-def show_suggestions(output_text_placeholder, option_placeholders, buttons, suggestions):
+def show_and_select_suggestions(output_text_placeholder, option_placeholders, buttons, suggestions):
     output_text_placeholder.write("*Did you mean...?*")
     for i, (word, _) in enumerate(suggestions):
-        buttons.append(
-            (option_placeholders[i], option_placeholders[i].button(word)))
+        buttons.append((option_placeholders[i],
+                        option_placeholders[i].button(word,
+                                                      on_click=update_input,
+                                                      args=(word, output_text_placeholder, option_placeholders))))
 
 
 def clear_elements(elements):
@@ -68,38 +68,40 @@ def clear_elements(elements):
         element.empty()
 
 
-def main():
-    k = 5
-    st.title('Word Correction using Levenshtein distance')
-    vocab = load_vocab('./google-10000-english-usa.txt')
-    if 'distance_cache' not in st.session_state:
-        st.session_state.distance_cache = {}
-    print('Empty input word 0')
+def init(num_of_suggestions):
     input_word_placeholder = st.empty()
-    print('Empty input word 1')
     output_text_placeholder = st.empty()
-    option_placeholders = [st.empty() for _ in range(k)]
+    option_placeholders = [st.empty() for _ in range(num_of_suggestions)]
     clear_placeholder = st.empty()
     buttons = []
-    print('Last word 0:', st.session_state.get('last_word', None))
-    input_word = input_word_placeholder.text_input('Input word')
-    st.write(input_word)
+    input_word = input_word_placeholder.text_input(
+        'Input word', key='input_word')
     clear_button = clear_placeholder.button('Clear suggestions', type='primary',
                                             on_click=clear_elements,
                                             args=([*option_placeholders, output_text_placeholder],))
+
+    return output_text_placeholder, option_placeholders, buttons, input_word, clear_button
+
+
+def main() -> None:
+    num_of_suggestions = 5
+
+    vocab = load_vocab('./google-10000-english-usa.txt')
+
+    if 'distance_cache' not in st.session_state:
+        st.session_state.distance_cache = {}
+
+    output_text_placeholder, option_placeholders, buttons, input_word, clear_button = init(
+        num_of_suggestions)
     if input_word and not clear_button:
-        print('Input word:', input_word)
-        print('Last word 1:', st.session_state.get('last_word', None))
-        st.session_state['last_word'] = input_word
-        print('Last word 2:', st.session_state.get('last_word', None))
         if input_word in vocab:
             st.write(f'Word *{input_word}* is correct')
+        # If correction is needed
         else:
-            suggestions = get_levenstein_distances(input_word, vocab, k)
-            show_suggestions(output_text_placeholder,
-                             option_placeholders, buttons, suggestions)
-            choose_corrected_word(input_word_placeholder,
-                                  output_text_placeholder, option_placeholders, buttons, suggestions)
+            suggestions = get_levenstein_distances(
+                input_word, vocab, num_of_suggestions)
+            show_and_select_suggestions(output_text_placeholder,
+                                        option_placeholders, buttons, suggestions)
 
 
 if __name__ == '__main__':
